0b5435e4afbbf984f9f4674a4b29e33e12668c46
Nicolas Bonamy <nicolas@bonamy.fr>
2024-11-08 16:32:59 -0600

    header name fix

--- src/utils/middlewares.ts
+++ src/utils/middlewares.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 
 // middleware to check for API_KEY header
 export const apiKeyMiddleware = (req: Request, res: Response, next: NextFunction): void => {
-  const apiKey = req.header('API_KEY');
+  const apiKey = req.header('API-KEY');
   if (apiKey !== process.env.AUTHORIZED_API_KEY) {
     res.status(401).json({ error: 'API_KEY header is missing or invalid' });
   } else {
