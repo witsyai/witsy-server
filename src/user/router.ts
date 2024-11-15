33be5a10fc52eac4d579e9088388a790b86a9f8b
Nicolas Bonamy <nicolas@bonamy.fr>
2024-11-15 08:58:22 -0600

    tweak

--- src/user/router.ts
+++ src/user/router.ts
@@ -22,6 +22,7 @@ router.get('/search', adminMiddleware, async (req: AuthedRequest, res) => {
       id: user.id,
       username: user.username,
       email: user.email,
+      subscriptionTier: user.subscriptionTier,
     })));
 
   } catch (error) {
